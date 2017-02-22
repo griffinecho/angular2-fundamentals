78ac83161d9a1628fa01979059ed4b9c45818cb8
thomasmalmberg <thomas.malmberg@live.com>
2017-02-22 23:43:03 +0100

    Routes, user module, link styling, lazy loading

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,24 +1,49 @@
 
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
+import { RouterModule } from '@angular/router'
 
+import {
+    EventsListComponent,
+    EventThumbnailComponent,
+    EventDetailsComponent,
+    CreateEventComponent,
+    EventService,
+    EventRouteActivator,
+    EventListResolver,
+} from './events/index'
 import { EventsAppComponent } from './events-appcomponent'
-import { EventsListComponent } from './events/events-list.component'
-import { EventThumbnailComponent } from './events/event-thumbnail.component'
 import { NavBarComponent } from './nav/navbar.component'
-import { EventService} from './events/shared/event.service'
-import { ToastrService} from './common/toastr.service'
+import { ToastrService } from './common/toastr.service'
+import { appRoutes } from './routes'
+import { Error404Component } from './errors/404.component'
 
 @NgModule({
-    imports: [BrowserModule],
+    imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
     declarations: [
         EventsAppComponent,
         EventsListComponent,
         EventThumbnailComponent,
-        NavBarComponent
+        EventDetailsComponent,
+        CreateEventComponent,
+        NavBarComponent,
+        Error404Component,
     ],
-    providers: [EventService, ToastrService],
+    providers: [
+        EventService,
+        ToastrService,
+        EventRouteActivator,
+        EventListResolver,
+        { provide: 'canDeactivateCreateEvent', useValue: checkDirtyState }
+    ],
     bootstrap: [EventsAppComponent]
 })
 
 export class AppModule { }
+
+function checkDirtyState(component: CreateEventComponent) {
+    if (component.isDirty) {
+        return window.confirm('You have not saved this event, do you really want to cancel?')
+    }
+    return true;
+}
---
+++ app/events/event-details/event-details.component.ts
@@ -0,0 +1,20 @@
+
+import { Component } from '@angular/core'
+import { EventService} from '../shared/event.service'
+import { ActivatedRoute } from '@angular/router'
+
+@Component({
+    templateUrl: '/app/events/event-details/event-details.component.html',
+    styleUrls: ['/app/events/event-details/event-details.component.css']
+})
+export class EventDetailsComponent {
+    event: any
+
+    constructor(private eventService: EventService, private route: ActivatedRoute){
+
+    }
+
+    ngOnInit(){
+        this.event = this.eventService.getEvent(+this.route.snapshot.params['id']);
+    }
+}
